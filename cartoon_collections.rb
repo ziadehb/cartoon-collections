0877051a3944ea29146da8d8342b9dea65df5b97
ziadehb <40444656+ziadehb@users.noreply.github.com>
2020-12-17 21:03:08 -0500

    Add files via upload

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,18 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each.with_index(1){|name, number| puts "#{number}. #{name}"}
+end 
+
+def summon_captain_planet(veggies_fruits)
+  veggies_fruits.map {|i| i.capitalize + "!"}
+
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any?{|n| n.length > 4}
+
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese.detect{|c| cheese_types.include?(c)}
 end
